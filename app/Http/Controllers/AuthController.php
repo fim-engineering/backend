1d1af5e4dec3e76f8d5c7a6d0d0f705cf9a10329
baguskah <dwiutamabagus@gmail.com>
2018-02-15 06:22:41 +0700

    Try

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
     public function signup(Request $request)
     {
       $user = User::all();
-      dd($request);
+      dd($request->email);
       $this->validate($request, [
         'email'=> 'required|unique:users',
         'password'=>'required',
