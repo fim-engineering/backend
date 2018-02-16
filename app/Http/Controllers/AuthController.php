27bc5a8bae23e81db2ed49ab3182ff1ab517733d
baguskah <dwiutamabagus@gmail.com>
2018-02-16 10:10:06 +0700

    .

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login','signup']]);
+        $this->middleware('auth:api', ['except' => ['login','signup', 'me']]);
     }
 
 
