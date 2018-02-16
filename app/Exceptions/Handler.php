089a9ff58fc3a6bd4f3e628d3269a34e6623a06b
baguskah <dwiutamabagus@gmail.com>
2018-02-16 17:14:22 +0700

    .

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,5 +51,15 @@ class Handler extends ExceptionHandler
         return parent::render($request, $exception);
     }
 
-
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['error' => 'Unauthenticatedd.'], 401);
+    }
 }
