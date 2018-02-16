fcd5d4ab41d2d822b72beaa03607fd146a6f2a13
baguskah <dwiutamabagus@gmail.com>
2018-02-16 09:56:26 +0700

    .

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -18,6 +18,6 @@ class ProfilController extends Controller
 
     public function index()
     {
-      dd("MASUK TANPA AUTHssss ");
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Http\Request;
 Route::group(['middleware' => ['api']],function(){
 
   Route::post('/signup', 'AuthController@signup');
-  Route::post('/login', 'AuthController@login')->name('login');
+  Route::post('/login', 'AuthController@login');
   Route::post('/logout', 'AuthController@logout');
   Route::post('/refresh', 'AuthController@refresh');
   Route::post('/me', 'AuthController@me');
