e3f2373d1b9060cc3b13dc71eefa232c85c9703b
baguskah <dwiutamabagus@gmail.com>
2018-02-28 21:37:06 +0700

    repair seeder

--- database/seeds/DbRegional.php
+++ database/seeds/DbRegional.php
@@ -4,11 +4,11 @@ use Illuminate\Database\Seeder;
 
 class DbRegional extends Seeder
 {
-    /
-      Run the database seeds.
-
-      @return void
-     /
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
     public function run()
     {
         DB::table('regionals')->insert(array(
