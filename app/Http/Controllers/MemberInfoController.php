82bde638d07bee65db08264df00b887c6df35d36
baguskah <dwiutamabagus@gmail.com>
2018-04-02 21:25:00 +0700

    add validation for broadcast

--- app/Http/Controllers/MemberInfoController.php
+++ app/Http/Controllers/MemberInfoController.php
@@ -153,10 +153,17 @@ class MemberInfoController extends Controller
 
          // ->paginate(20);
 
+
          foreach ($all_submit as $key => $value) {
            $usersd = User::find($value->id);
-           $usersd->send_broadcast = 1; // regional
-           $usersd->save();
+
+           $validation = $this->user_validation($usersd->email);
+
+           if ($validation['null'] <5) {
+             $usersd->send_broadcast = 1; // regional
+             $usersd->save();
+           }
+
          }
 
          dd("success");
@@ -165,5 +172,161 @@ class MemberInfoController extends Controller
            'user_data' => $all_submit,
            'code' => 200,
          ]);
+    }
+
+    // Hanya yang terlihat niat
+
+    public function user_validation($email)
+    {
+
+        $user = User::where('email', $email)->first();
+        $regional_id = $user->regional_id;
+
+
+        $profile      = $user->profiles;
+        $achievement  = $user->achievement_bests;
+        $personality  = $user->personality;
+        $meandfim     = $user->me_and_fim;
+        $null =0;
+        $point=0;
+
+
+
+        if ($profile     !== null) {
+          $column[] = array(
+          'Nama Lengkap'  => $profile->full_name,
+          'Angkatan'  => $profile->generation,
+          'Jurusan'  => $profile->majors,
+          'Institusi'  => $profile->institution,
+          'Alamat'  => $profile->address,
+          'Kota'  => $profile->city,
+          'Nomor Handphone'  => $profile->phone,
+          'Jenis Kelamin'  => $profile->gender,
+          'Foto Profil'  => $profile->photo_profile_link,
+          'Foto KTP'  => $profile->ktp_link,
+          'Golongan Darah'  => $profile->blood,
+          'Tanggal Lahir'  => $profile->born_date,
+          'Tempat Lahir'  => $profile->born_city,
+          'Link Facebook'  => $profile->facebook,
+          'Username Instagram'  => $profile->instagram,
+          'Riwayat Penyakit'  => $profile->disease_history,
+          'Agama'  => $profile->religion,
+        );
+
+        if ($achievement !== null) {
+          $column[] = array(
+            /**
+             * Pencapaian Pertama
+             */
+            'Nama Pencapaian Pertama' => $achievement->achievement,
+            'Tanggal Awal Pencapaian Pertama' => $achievement->date_from,
+            'Tanggal Akhir Pencapaian Pertama' => $achievement->date_end,
+            'Jenis Posisi Pencapaian Pertama' => $achievement->position_name,
+            'Email Atasan Pencapaian Pertama' => $achievement->email_leader,
+            'Nomor Telepon Atasan Pencapaian Pertama' => $achievement->phone_leader,
+            'Deskripsi Pencapaian Pertama' => $achievement->description,
+
+            /**
+             * Pencapaian Kedua
+             */
+
+             'Nama Pencapaian Ke-2' => $achievement->achievement_2,
+             'Tanggal Awal Pencapaian Ke-2' => $achievement->date_from_2,
+             'Tanggal Akhir Pencapaian Ke-2' => $achievement->date_end_2,
+             'Jenis Posisi Pencapaian Ke-2' => $achievement->position_name_2,
+             'Email Atasan Pencapaian Ke-2' => $achievement->email_leader_2,
+             'Nomor Telepon Atasan Pencapaian Ke-2' => $achievement->phone_leader_2,
+             'Deskripsi Pencapaian Ke-2' => $achievement->description_2,
+
+             /**
+              * Pencapaian Ketiga
+              */
+
+
+              'Nama Pencapaian Ke-3' => $achievement->achievement_3,
+              'Tanggal Awal Pencapaian Ke-3' => $achievement->date_from_3,
+              'Tanggal Akhir Pencapaian Ke-3' => $achievement->date_end_3,
+              'Jenis Posisi Pencapaian Ke-3' => $achievement->position_name_3,
+              'Email Atasan Pencapaian Ke-3' => $achievement->email_leader_3,
+              'Nomor Telepon Atasan Pencapaian Ke-3' => $achievement->phone_leader_3,
+              'Deskripsi Pencapaian Ke-3' => $achievement->description_3,
+          );
+        }
+
+        if ($personality !== null) {
+          $column[] = array(
+            /**
+             * Personality Check
+             */
+            'Tipe MBTI' => $personality->mbti,
+            '3 Kekuatan' => $personality->strength,
+            '3 Kelemahan' => $personality->weakness,
+            'Performance Paling Baik saat di Organisasi' => $personality->best_performance,
+            'Tokoh Idola Ke-1' => $personality->role_model,
+            'Tokoh Idola Ke-2' => $personality->role_model_2,
+            'Tokoh Idola Ke-3' => $personality->role_model_3,
+            'Masalah Anak Muda Ke-1' => $personality->problem_solver,
+            'Masalah Anak Muda Ke-2' => $personality->problem_solver_2,
+            'Masalah Anak Muda Ke-3' => $personality->problem_solver_3,
+            'Nilai Kondisi Anak Muda (Cinta Kasih)' => $personality->cintakasih,
+            'Nilai Kondisi Anak Muda (Integritas)' => $personality->integritas,
+            // 'Nilai Kondisi Anak Muda (Kebersahajaan)' => $personality->Kebersahajaan,
+            'Nilai Kondisi Anak Muda (Totalitas)' => $personality->totalitas,
+            'Nilai Kondisi Anak Muda (Solidaritas)' => $personality->solidaritas,
+            'Nilai Kondisi Anak Muda (Keadilan)' => $personality->keadilan,
+            'Nilai Kondisi Anak Muda (Keteladanan)' => $personality->keteladanan,
+          );
+        }
+
+        if ($meandfim    !== null) {
+
+          $column[] = array(
+            'Mengetahui FIM dari Mana' => $meandfim->fim_reference,
+            'Mengapa Kamu Ingin Bergabung FIM' => $meandfim->why_join_fim,
+            'Skill / Sumberdaya Apa yang Bisa Dikontribusikan' => $meandfim->skill_for_fim,
+            'Bakat yang Akan Ditampilkan di Api Ekspresi' => $meandfim->performance_apiekspresi,
+            );
+          }
+
+
+            foreach ($column as $key => $table)
+            {
+              $data[] = $table;
+
+              /**
+               * Seluruh Notif dan Null
+               */
+              foreach ($table as $key => $value) {
+                if ($value == NULL) {
+                  $null++;
+                  $notif[]= "Kolom $key belum diisi";
+                }else {
+                  $point++;
+                }
+              }
+            }
+
+
+            if (!isset($notif)) {
+              $status = true;
+              $notif = "Data Lengkap";
+            }else {
+              $status = false;
+            }
+
+            $array = array(
+              'null' =>$null,
+              'notif' =>$notif,
+              'status' =>$status,
+            );
+
+            return $array;
+            // return response()->json([
+            //   // 'data' => $data,
+            //   'null' =>$null,
+            //   'notif' =>$notif,
+            //   'status' =>$status,
+            // ]);
+        }
     }
 }
