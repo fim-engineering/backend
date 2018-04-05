bc515b7407cda2addbc6353754c2326d442bb6ba
baguskah <dwiutamabagus@gmail.com>
2018-04-06 01:27:11 +0700

    Excel

---
+++ app/Exports/invoicesExport.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Support\Facades\DB;
+use Excel;
+use App\User;
+
+
+
+class InvoicesExport implements FromCollection
+{
+
+    // use Exportable;
+    public $regional;
+
+    public function __construct($regional)
+    {
+        $this->regional = $regional;
+
+    }
+
+    public function collection()
+    {
+
+      // $data =  DB::table('users')
+      //   ->join('achievement_bests', 'users.id', '=', 'achievement_bests.user_id')
+      //   ->join('me_and_fims', 'users.id', '=', 'me_and_fims.user_id')
+      //   ->join('personalities', 'users.id', '=', 'personalities.user_id')
+      //   ->join('profiles', function ($join) {
+      //          $join->on('users.id', '=', 'profiles.user_id')
+      //               ->where([['profiles.city', $this->regional],['users.final_submit', 1]])->whereNull('users.deleted_at');
+      //      })
+      //      ->get();
+
+      $data = User::all();
+
+
+           return $data;
+    }
+
+}
