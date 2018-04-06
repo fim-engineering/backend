4bd55dbe245a57aa27357eb76224795ea67f54a0
baguskah <dwiutamabagus@gmail.com>
2018-04-06 07:49:45 +0700

    fix excel

--- app/Exports/invoicesExport.php
+++ app/Exports/invoicesExport.php
@@ -26,8 +26,18 @@ class InvoicesExport implements FromCollection
     public function collection()
     {
 
-      $members =  DB::table('users')
-        ->join('achievement_bests', 'users.id', '=', 'achievement_bests.user_id')
+      // $members =  DB::table('users')
+      //   ->join('achievement_bests', 'users.id', '=', 'achievement_bests.user_id')
+      //   ->join('me_and_fims', 'users.id', '=', 'me_and_fims.user_id')
+      //   ->join('personalities', 'users.id', '=', 'personalities.user_id')
+      //   ->join('profiles', function ($join) {
+      //          $join->on('users.id', '=', 'profiles.user_id')
+      //               ->where([['profiles.city', $this->regional],['users.final_submit', 1]])->whereNull('users.deleted_at');
+      //      })
+      //      ->get();
+      //      $members= json_decode( json_encode($members), true);
+
+      $members =  User::join('achievement_bests', 'users.id', '=', 'achievement_bests.user_id')
         ->join('me_and_fims', 'users.id', '=', 'me_and_fims.user_id')
         ->join('personalities', 'users.id', '=', 'personalities.user_id')
         ->join('profiles', function ($join) {
@@ -35,6 +45,7 @@ class InvoicesExport implements FromCollection
                     ->where([['profiles.city', $this->regional],['users.final_submit', 1]])->whereNull('users.deleted_at');
            })
            ->get();
+    
 
            return $members;
     }
